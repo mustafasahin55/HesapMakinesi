802920c053e366827f4a94999e5e5a610f7a507e
Mustafa Şahin <mustafasahin8484@gmail.com>
2024-08-23 16:19:28 +0300

    v1.0

--- app/src/main/java/com/example/hesapmakinesi/Anasayfa.kt
+++ app/src/main/java/com/example/hesapmakinesi/Anasayfa.kt
@@ -1,27 +1,32 @@
 package com.example.hesapmakinesi
-
-import android.graphics.Paint.Align
-import android.util.Log
+import android.annotation.SuppressLint
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.hesapmakinesi.ui.theme.bgColor
+import com.example.hesapmakinesi.ui.theme.darkButtonText
+import com.example.hesapmakinesi.ui.theme.lightDarkButton
+import com.example.hesapmakinesi.ui.theme.worksans
 
 
 @Composable
@@ -29,116 +34,163 @@ fun Anasayfa() {
     val configuration = LocalConfiguration.current
     val genislik = configuration.screenWidthDp
     val yukseklik = configuration.screenHeightDp
-    //Log.e("test","$genislik-$yukseklik") //411-707
-    val kG = 0.12165 * genislik
-    val kY = 0.07072 * yukseklik
+    val kG = (0.12165 * genislik * 0.7)
+    val kY = (0.07072 * yukseklik * 0.7)
 
-    var ekran = remember {
-        mutableStateOf("")
+    var ekran = remember { mutableStateOf("") }
+    var oncekiIslem = remember { mutableStateOf("") }
+    val scrollState = rememberScrollState()
+    LaunchedEffect(ekran.value) {
+
+        scrollState.animateScrollTo(scrollState.maxValue)
     }
 
 
-
-    Scaffold(
-
-
-    ) { paddingValues ->
+    Scaffold { paddingValues ->
 
         Column(
             modifier = Modifier
                 .fillMaxSize()
-                .padding(paddingValues),
+                .padding(paddingValues)
+                .background(bgColor),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
         ) {
 
-            Box(modifier = Modifier.size((kG*8.1).dp,100.dp).background(color = Color.Red).padding(10.dp).align(Alignment.End)) {
-                Text(text = ekran.value, color = Color.White, modifier = Modifier
-                    .fillMaxWidth() // Ensure the Text takes the full width of the Box
-                    .align(Alignment.CenterEnd))
+            Column(
+                modifier = Modifier
+                    .padding(paddingValues)
+                    .background(bgColor),
+                horizontalAlignment = Alignment.End,
+                verticalArrangement = Arrangement.Bottom
+            ) {
+                Box(
+                    modifier = Modifier
+                        .size((kG * 8.1).dp, 100.dp)
+                        .background(color = bgColor)
+                        .padding(0.dp)
+                        .align(Alignment.End),
+
+                    contentAlignment = Alignment.CenterEnd
+                ) {
+                    Text(
+                        text = oncekiIslem.value, color = lightDarkButton,
+                        fontFamily = worksans,
+                        fontSize = 37.sp,
+                        maxLines = 1,
+                        )
+                }
+
+                Box(
+                    modifier = Modifier
+                        .size((kG * 8.1).dp, 100.dp)
+                        .background(color = bgColor)
+                        .padding(0.dp)
+                        .align(Alignment.End),
+
+                    contentAlignment = Alignment.CenterEnd
+                ) {
+
+                    Row(
+                        modifier = Modifier
+                            .horizontalScroll(scrollState)
+                            .wrapContentWidth(Alignment.End)
+                            .background(bgColor),
+                        horizontalArrangement = Arrangement.End
+                    ) {
+                        Text(
+                            text = ekran.value,
+                            color = darkButtonText,
+                            fontFamily = worksans,
+                            fontSize = 50.sp,
+                            maxLines = 1,
+                            overflow = TextOverflow.Visible
+                        )
+                    }
+                }
             }
 
-
-            Row(
-                horizontalArrangement = Arrangement.Start,
-
-
-                ) {
-                Column() {
-
+            Row(horizontalArrangement = Arrangement.Start) {
+                Column {
                     Row {
-                        ButtonChip(icerik = "9", h = (kY * 2).toInt(), (kG * 2).toInt()) {
+                        ButtonChip(icerik = "9", h = (kY * 2).toInt(), (kG * 2).toInt(), 0) {
                             ekran.value += "9"
                         }
-                        ButtonChip(icerik = "8", (kY * 2).toInt(), (kG * 2).toInt()) {
+                        ButtonChip(icerik = "8", (kY * 2).toInt(), (kG * 2).toInt(), 0) {
                             ekran.value += "8"
                         }
-                        ButtonChip(icerik = "7", (kY * 2).toInt(), (kG * 2).toInt())
-                        {
+                        ButtonChip(icerik = "7", (kY * 2).toInt(), (kG * 2).toInt(), 0) {
                             ekran.value += "7"
                         }
                     }
                     Row {
-                        ButtonChip(icerik = "6", (kY * 2).toInt(), (kG * 2).toInt())
-                        {
+                        ButtonChip(icerik = "6", (kY * 2).toInt(), (kG * 2).toInt(), 0) {
                             ekran.value += "6"
                         }
-                        ButtonChip(icerik = "5", (kY * 2).toInt(), (kG * 2).toInt())
-                        {
+                        ButtonChip(icerik = "5", (kY * 2).toInt(), (kG * 2).toInt(), 0) {
                             ekran.value += "5"
                         }
-                        ButtonChip(icerik = "4", (kY * 2).toInt(), (kG * 2).toInt())
-                        {
+                        ButtonChip(icerik = "4", (kY * 2).toInt(), (kG * 2).toInt(), 0) {
                             ekran.value += "4"
                         }
                     }
                     Row {
-                        ButtonChip(icerik = "3", (kY * 2).toInt(), (kG * 2).toInt())
-                        {
+                        ButtonChip(icerik = "3", (kY * 2).toInt(), (kG * 2).toInt(), 0) {
                             ekran.value += "3"
                         }
-                        ButtonChip(icerik = "2", (kY * 2).toInt(), (kG * 2).toInt())
-                        {
+                        ButtonChip(icerik = "2", (kY * 2).toInt(), (kG * 2).toInt(), 0) {
                             ekran.value += "2"
                         }
-                        ButtonChip(icerik = "1", (kY * 2).toInt(), (kG * 2).toInt())
-                        {
+                        ButtonChip(icerik = "1", (kY * 2).toInt(), (kG * 2).toInt(), 0) {
                             ekran.value += "1"
                         }
                     }
                     Row {
-                        ButtonChip(icerik = "0", (kY * 2).toInt(), (kG * 4.02).toInt())
-                        {
+                        ButtonChip(icerik = "0", (kY * 2).toInt(), (kG * 4.66).toInt(), 0) {
                             ekran.value += "0"
                         }
-                        ButtonChip(icerik = ",", ((kY * 2)).toInt(), (kG * 2).toInt())
-                        {
-                            ekran.value += ","
+                        ButtonChip(icerik = ".", (kY * 2).toInt(), (kG * 2).toInt(), 0) {
+                            ekran.value += "."
+                        }
+                    }
+                }
+                Column {
+                    ButtonChip(icerik = "C", (kY * 2).toInt(), (kG * 2).toInt(), 2) {
+                        ekran.value = ""
+                        oncekiIslem.value = ""
+                    }
+                    ButtonChip(icerik = "Sil", (kY * 2).toInt(), (kG * 2).toInt(), 1) {
+                        ekran.value = ekran.value.dropLast(1)
+                    }
+                    ButtonChip(icerik = "+", (kY * 2).toInt(), (kG * 2).toInt(), 1) {
+                        if (ekran.value.isNotEmpty() && ekran.value.last() != '+') {
+                            ekran.value += "+"
+                        }
+                    }
+                    ButtonChip(icerik = "=", (kY * 2).toInt(), (kG * 2).toInt(), 1) {
+                        oncekiIslem.value = ekran.value
+                        ekran.value = calculateResult(ekran.value)
+
+
+
                         }
 
                     }
                 }
-                Column() {
-                    ButtonChip(icerik = "Sil", ((kY * 2)).toInt(), (kG * 2).toInt())
-                    {
-
-                    }
-                    ButtonChip(icerik = "+", ((kY * 2)).toInt(), (kG * 2).toInt())
-                    {
-                        ekran.value += "+"
-                    }
-                    ButtonChip(icerik = "=", ((kY * 4.02)).toInt(), (kG * 2).toInt())
-                    {
-
-                    }
-                }
-
-
             }
-
-
         }
-
-
     }
 
 
+@SuppressLint("DefaultLocale")
+fun calculateResult(expression: String): String {
+    return try {
+        val sanitizedExpression = expression.replace(",", ".")
+        val result = sanitizedExpression.split("+").sumOf { it.toDouble() }
+
+        String.format("%.2f", result) // Rounded to two decimal places
+
+    } catch (e: Exception) {
+        "Error"
+    }
 }
--- app/src/main/java/com/example/hesapmakinesi/ButtonChip.kt
+++ app/src/main/java/com/example/hesapmakinesi/ButtonChip.kt
@@ -10,43 +10,60 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.hesapmakinesi.ui.theme.blueButton
+import com.example.hesapmakinesi.ui.theme.blueButtonBorder
+import com.example.hesapmakinesi.ui.theme.darkButton
+import com.example.hesapmakinesi.ui.theme.darkButtonText
+import com.example.hesapmakinesi.ui.theme.lightDarkButton
 
 @Composable
-fun ButtonChip(icerik: String, h: Int, w: Int, onClick: () -> Unit) {
+fun ButtonChip(icerik: String, h: Int, w: Int,stil:Int, onClick: () -> Unit) {
 
     Box(
         modifier = Modifier
-            .padding(1.dp)
-            .background(color = Color.DarkGray, shape = RoundedCornerShape(6.dp))
+            .padding(12.dp)
+            .background(
+                color = if (stil == 0) darkButton else (if (stil == 2) lightDarkButton else blueButton),
+                shape = RoundedCornerShape((if (icerik == "0") ((h / 3).dp) else ((w / 3).dp)))
+            )
             .size(height = h.dp, width = w.dp)
             .border(
-                2.dp, Color.Gray,
-                RoundedCornerShape(10)
+                1.dp,
+                if (stil == 0) darkButton else (if (stil == 2) lightDarkButton else blueButtonBorder),
+
+                RoundedCornerShape(33)
             )
             .clickable {
-                if (icerik == "=" || icerik == "Sil") {
-                    //
-                } else {
-                    onClick()
-                }
+                onClick()
 
             }
     ) {
-        Text(
-            text = icerik,
-            color = Color.Gray,
-            fontSize = 50.sp,
-            modifier = Modifier.fillMaxWidth(),
-            textAlign = TextAlign.Center,
-        )
+        if (icerik == "Sil"){
+            Icon(painter = painterResource(id = R.drawable.baseline_backspace_24), contentDescription ="" ,
+                modifier = Modifier.align(Alignment.Center).size(35.dp))
+        }else{
+            Text(
+                text = icerik,
+                color = darkButtonText,
+                fontSize = 32.sp,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .align(Alignment.Center),
+                textAlign = TextAlign.Center,
+            )
+        }
+
+
     }
 
 }
